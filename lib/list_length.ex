ffa5a377149b458bf155cef1ad93558dbc5bea2b
dalissongabriel <alissongabrieldino@gmail.com>
2021-11-09 06:40:20 -0300

    feat(list_length): add recursive function to count elements inside an list

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+  defp count([], counter), do: counter
+
+  defp count([_ | tail], counter) do
+    counter = counter + 1
+    count(tail, counter)
   end
 end
